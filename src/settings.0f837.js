bf9a999ddf0e9b892b0dc37523abb2704f861686
o0o0oallen <fansiscoson71allen@gmail.com>
2023-07-28 10:39:03 +0700

    upload

---
+++ src/settings.0f837.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/login.fire",orientation:"",jsList:["assets/scripts/commons/lodash.72bc9.js"],bundleVers:{internal:"468ae",resources:"494b8",main:"b7191"}};
